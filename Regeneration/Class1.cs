eb3fc4ae6499526708890d05e19633853d5b9cb1
MisinformedOwl <60757710+MisinformedOwl@users.noreply.github.com>
2024-05-09 16:08:32 +0100

    Add files via upload

---
+++ Regeneration/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Regeneration
+{
+    public class CompSanguophageRegeneration : ThingComp
+    {
+        public override void CompTick()
+        {
+            base.CompTick();
+            Log.Message("This is a test");
+        }
+    }
+}
