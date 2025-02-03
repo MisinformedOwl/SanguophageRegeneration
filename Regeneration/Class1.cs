59d0a66bfffecc1f23aa494188fdf3ddb812d690
MisinformedOwl <jaydean1311@gmail.com>
2025-02-03 20:19:18 +0000

    New files updated as requested

--- Regeneration/Class1.cs
+++ Regeneration/Class1.cs
@@ -6,55 +6,108 @@ using System.Threading.Tasks;
 using RimWorld;
 using Verse;
 using HarmonyLib;
+using LudeonTK;
+using System.Xml;
+using System.IO;
 
 namespace Regeneration
 {
     [StaticConstructorOnStartup]
     public static class DeathRestRegenerate
     {
+        public static int requiredTicks;
+
         static DeathRestRegenerate()
         {
-            Log.Message("Harmony loaded for Sanguophage Regeneration.");
-            new Harmony("rimworld.mod.misinformedOwl.sanguophageregeneration").PatchAll();
-        }
-    }
-
-    [HarmonyPatch(typeof(Gene_Deathrest))]
-    [HarmonyPatch(nameof(Gene_Deathrest.TickDeathresting))]
-    class DeathrestPatch
-    {
-        static void Postfix(Gene_Deathrest __instance)
-        {
-            Pawn pawn = __instance.pawn;
-            Building_Bed building_Bed = pawn.CurrentBed();
-            if (building_Bed != null)
+            try
             {
-                if (__instance.deathrestTicks % 12000 == 11999)
-                {
-                    HediffSet missingBodyParts = pawn.health.hediffSet;
-                    if (missingBodyParts != null)
-                    {
-                        List<Hediff_MissingPart> missingPartsToRemove = new List<Hediff_MissingPart>();
+                Log.Message("Harmony loaded for Sanguophage Regeneration.");
+                XmlDocument doc = new XmlDocument();
 
-                        foreach (Hediff part in missingBodyParts.hediffs)
+                // Define paths
+                string localPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Mods/Regeneration/CHANGE REGEN HERE.xml");
+                string steamWorkshopPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "steamapps/workshop/content/294100/YourModID/CHANGE REGEN HERE.xml");
+
+                // Check which path exists
+                if (File.Exists(localPath))
+                {
+                    Log.Error("Loading Local.");
+                    doc.Load(localPath);
+                }
+                else if (File.Exists(steamWorkshopPath))
+                {
+                    Log.Error("Loading workshop.");
+                    doc.Load(steamWorkshopPath);
+                }
+                else
+                {
+                    Log.Error("XML configuration file not found in either local or Steam Workshop paths.");
+                    return;
+                }
+
+                XmlNode node = doc.SelectSingleNode("/Config/requiredTicks");
+                if (node != null)
+                {
+                    requiredTicks = int.Parse(node.InnerText);
+                    Log.Message($"requiredTicks set to: {requiredTicks}");
+                }
+                else
+                {
+                    Log.Error("Node /Config/requiredTicks not found in the XML.");
+                }
+
+                new Harmony("rimworld.mod.misinformedOwl.sanguophageregeneration").PatchAll();
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"An error occurred: {ex.Message}");
+            }
+        }
+
+        [HarmonyPatch(typeof(Gene_Deathrest))]
+        [HarmonyPatch(nameof(Gene_Deathrest.TickDeathresting))]
+        class DeathrestPatch
+        {
+            static void Postfix(Gene_Deathrest __instance)
+            {
+                Pawn pawn = __instance.pawn;
+                Building_Bed building_Bed = pawn.CurrentBed();
+                if (building_Bed != null)
+                {
+                    if (__instance.deathrestTicks % DeathRestRegenerate.requiredTicks == DeathRestRegenerate.requiredTicks - 1)
+                    {
+                        HediffSet missingBodyParts = pawn.health.hediffSet;
+                        if (missingBodyParts != null)
                         {
-                            if (part is Hediff_MissingPart missingPart)
+                            List<Hediff_MissingPart> missingPartsToRemove = new List<Hediff_MissingPart>();
+
+                            foreach (Hediff part in missingBodyParts.hediffs)
                             {
-                                missingPartsToRemove.Add(missingPart);
+                                if (part is Hediff_MissingPart missingPart)
+                                {
+                                    missingPartsToRemove.Add(missingPart);
+                                }
                             }
-                        }
-                        if (missingPartsToRemove.Count > 0)
-                        {
-                            pawn.health.RemoveHediff(missingPartsToRemove[0]);
-                            if (missingPartsToRemove.Count == 1)
+                            if (missingPartsToRemove.Count > 0)
                             {
-                                Messages.Message($"{pawn.Name} Has regenerated all limbs.", pawn, MessageTypeDefOf.PositiveEvent);
+                                foreach (Hediff_MissingPart part in missingPartsToRemove)
+                                {
+                                    string partName = part.Part.Label;
+                                    if (partName != "left lung" && partName != "right lung" && partName != "left kidney" && partName != "right kidney")
+                                    {
+                                        pawn.health.RemoveHediff(part);
+                                        break;
+                                    }
+                                }
+                                if (missingPartsToRemove.Count == 1)
+                                {
+                                    Messages.Message($"{pawn.Name} has regenerated all limbs.", pawn, MessageTypeDefOf.PositiveEvent);
+                                }
                             }
                         }
                     }
                 }
             }
-
         }
     }
 }
